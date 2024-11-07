6f76cbd4cf3ca51f732add21e2b2d6530c443c33
Olexandra <vt241_goa@student.ztu.edu.ua>
2024-11-07 23:58:25 +0200

    correction z

--- LinearExpressions1/Program.cs
+++ LinearExpressions1/Program.cs
@@ -15,3 +15,4 @@ double x, y, z, r;
 
 x = ((a + 2 * b - c + d) / (c * d)) + ((a + b) / (c - d)) - ((a * a) / (b * b));
 y = (5 * (a + b) / (c - d) / (0.5 * c)) + (b * b * ((a * a - b * b) / (b - a)));
+z = (Math.Pow((x * x - 2 * x), 3) - 4 * ((x * x * x * x) + 1) * (1 - b)) / (5 * a + 3 * b);
